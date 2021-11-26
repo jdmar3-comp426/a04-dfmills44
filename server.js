af01437b84ef511dd1db60bdf6bdc0bbc35301b1
dfmills44 <dfmills@live.unc.edu>
2021-11-26 17:26:15 -0500

    Draft 8

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.patch("/app/update/user/:id", (req, res) => {
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
-	res.status(200).json(stmt);
+	const info = stmt.run();
+	res.status(200).json({"message:":info.changes + " record deleted: ID " + info.lastInsertRowid});
 })
 // Default response for any other request
 app.use(function(req, res){
