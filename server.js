3caaa63d45cb2b16cfebfa497025247f2ea6d768
dfmills44 <dfmills@live.unc.edu>
2021-11-26 16:49:54 -0500

    Draft 4

--- server.js
+++ server.js
@@ -24,8 +24,11 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {
-	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run();
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
+	const info = stmt.run();
 	res.status(201).json(stmt);
+
+	console.log(info.changes);
 })
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
