19874bf379a0eae7929611fdc0fe341e7590b19a
dfmills44 <dfmills@live.unc.edu>
2021-11-26 16:29:47 -0500

    Draft 2

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get();
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
