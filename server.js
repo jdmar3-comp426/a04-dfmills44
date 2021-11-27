2a24cd8a431b57992fe62528309992486872c248
dfmills44 <dfmills@live.unc.edu>
2021-11-27 18:49:02 -0500

    Draft 10

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, req.body.pass);
-	res.status(201).json("We made a new database entry.");
+	res.status(201).json({"message": info.changes + " record created: ID "+ info.lastInsertRowid + " (201)"});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
