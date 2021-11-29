a9b3e80b433f452b7b3216a65e222d3f959cb8ec
dfmills44 <dfmills@live.unc.edu>
2021-11-28 21:36:47 -0500

    Draft 22

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get("/app/user/:id", (req, res) => {
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	res.status(200).json(stmt);
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, req.body.pass, req.params.id);
+	res.status(200).json({"message":"Post Updated."});
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
